bf9055964f0c355891f0ea057ee5003481fac475
Umidjon Mirzaaliyev Web <115705707+UmidjonMirzaaliyev@users.noreply.github.com>
2024-06-10 08:01:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,263 @@
+// 1-masala
+
+// let arr = [2, 4, 5, 11, 16, 20]
+// let newArr = []
+
+// arr.forEach(function(value){
+
+//     newArr.push(value * value)
+// })
+// console.log(newArr);
+
+// 2-masala
+
+// let arr = [-1, -4, 20, 13, -6, 4, -11]
+// let musbatArr = []
+
+// arr.forEach((value) =>{
+//     if(value > 0){
+//         musbatArr.push(value)
+//     }
+// })
+// console.log(musbatArr);
+
+// 3-masala
+
+// let arr = [2, 4, 5, 11, 16, 20]
+
+// arr.forEach((value) =>{
+//     console.log(value * 2);
+// })
+
+// 4-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+// let sum = 0;
+
+// arr.forEach((value) => {
+//   sum += value;
+// });
+
+// console.log(sum);
+
+// 5-masala
+
+// let arr = [-1, -4, 20, 13, -6, 4, -11];
+
+// let musbat2 = arr.filter((value) => value > 0);
+// console.log(musbat2);
+
+// 6-masala
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let juft = arr.filter((value) => value % 2 == 0);
+// console.log(juft);
+
+//7-masala
+
+// let str1 = ["BMW", "MERCEDEZ", "AUDI", "LI 9", "MATIZ", "NEXIA", "TICO"]
+
+// let str2 = str1.filter(value => value.length < 5)
+// console.log(str2);
+
+// 8-masala
+
+// let yosh = [19, 7, 12, 17, 24, 40, 34, 20, 15];
+
+// let yosh2 = yosh.filter((value) => value > 18);
+// console.log(yosh2);
+
+// 9-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+
+// let arr2 = arr.map((value) => value * value);
+// console.log(arr2);
+
+//10-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+
+// let arr2 = arr.map((value) => value + 5);
+// console.log(arr2);
+
+// 11-masala
+
+// let str = ["bmw", "mercedez", "audi", "li 9", "matiz", "nexia", "tiko"];
+
+// let str2 = str.map((words) => words.toUpperCase());
+// console.log(str2);
+
+// 12-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+
+// let arr2 = arr.map(value => Math.sqrt(value))
+// console.log(arr2);
+
+// 13-masala
+
+// let arr = [1, 2, 3, -4, 5];
+
+// let manfiySon = arr.some(value => value < 0)
+// console.log(manfiySon);
+
+// 14-masala
+
+// let str = ["bmw", "mercedezbenz", "audi", "li 9", "matiz", "nexia", "tiko", "lamborghini", ];
+// let str2 = str.some(value => value.length > 10)
+
+// console.log(str2);
+
+// 15-masala
+
+// let arr = [1, 2, 3, -4, 5];
+
+// let arr2 = arr.every((value) => value > 0);
+// console.log(arr2);
+
+// 16-masala
+
+// let str1 = ["BMW", "MERCEDEZ", "AUDI", "LI 9", "MATIZ", "NEXIA", "tico"];
+
+// let str2 = str1.every((value) => value == value.toUpperCase());
+
+// console.log(str2);
+
+// 17-masala
+
+// let str1 = ["BMW", "MERCEDEZ", "AUDI", "LI 9", "MATIZ", "NEXIA", "tico"];
+
+// let find = str1.includes("BMW")
+//  console.log(find);
+
+// 18-masala
+
+// let str1 = [
+//   "BMW",
+//   "NEXIA",
+//   "MERCEDEZ",
+//   "AUDI",
+//   "LI 9",
+//   "MATIZ",
+//   "NEXIA",
+//   "tico",
+// ];
+
+// let find = str1.indexOf("NEXIA");
+// console.log(find);
+
+// 19-masala
+
+// let str1 = [
+//     "BMW",
+//     "NEXIA",
+//     "MERCEDEZ",
+//     "AUDI",
+//     "LI 9",
+//     "MATIZ",
+//     "NEXIA",
+//     "tico",
+//   ];
+
+//   let find = str1.lastIndexOf("NEXIA");
+//   console.log(find);
+
+// 20-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+// let find = arr.find(value => value > 9 && value < 100)
+
+// console.log(find);
+
+// 21-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+// let find = arr.findIndex(value => value > 9 && value < 100)
+// console.log(find);
+
+//22-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+// let find = arr.findLast(value => value > 9 && value < 100)
+
+// console.log(find);
+
+// 23-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+// let find = arr.findLastIndex(value => value > 9 && value < 100)
+// console.log(find);
+
+// 24-masala
+
+// let str1 = [
+//   "BMW",
+//   "NEXIA",
+//   "MERCEDEZ",
+//   "AUDI",
+//   "LI 9",
+//   "MATIZ",
+//   "NEXIA",
+//   "tico",
+// ];
+
+// let reStr = str1.reverse()
+// console.log(reStr);
+
+// 25-masala
+
+// let arr = [21, 14, 50, 17, 16, 3];
+
+// let arr2 = arr.sort((a, b) =>{
+//     return a - b
+// })
+
+// console.log(arr2);
+
+// 26-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+
+// arr.forEach(function(value){
+//     console.log(value * 2);
+// })
+
+// 27-masala
+
+// let arr = [2, 4, -5, 11, -16, 20];
+
+// let arr2 = arr.filter((value) => value < 0);
+// console.log(arr2);
+
+// 28-masala
+
+// let arr = [2, 4, 5, 11, 16, 20];
+
+// let arr2 = arr.map((value) =>  value * value)
+// console.log(arr2);
+
+// 29-masala
+
+// let arr = [1, 2, 3, -4, 5];
+
+// let manfiySon = arr.some(value => value < 0)
+// console.log(manfiySon);
+
+// 30-masala
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let musbatSon = arr.every((value) => value > 0);
+// console.log(musbatSon);
+
+// 31-masala
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// let arr2 = arr.map(value => value * 3)
+
+// let arr3 = arr2.filter((value) => value > 10 )
+// console.log(arr3);
+
